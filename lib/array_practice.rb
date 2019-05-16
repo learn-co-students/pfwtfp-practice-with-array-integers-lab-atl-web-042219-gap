58a25e7cc4f3897516bbf170ff82adb4f03b9a8f
github username <github email address>
2019-05-16 15:01:54 -0400

    Done.

--- lib/array_practice.rb
+++ lib/array_practice.rb
@@ -1,39 +1,55 @@
+require "pry"
 array_of_integers = *0...50
 
 def all_odds(array)
   # return all odd numbers from the input array
+    array.select{|digit| digit % 2 == 1}
 end
 
 def all_evens(array)
   # return all even numbers from the input array
+  array.select{|digit| digit % 2 == 0}
 end
 
 def all_squares(array)
   # return the square of all numbers from the input array
+  array.map{|digit| digit*digit}
 end
 
 def first_square_greater_than_350(array)
   # return the first number from the input array whose square is greater than 350
+  array.find{|number| number*number > 350}
+  # array.find do |number|
+  #   number * number > 350 
+  # end
 end
 
 def all_squares_greater_than_350(array)
   # return all the numbers from the input array whose square is greater than 350
+  array.select{|number| number * number > 350}
 end
 
 def all_cubes(array)
   # return the cube of all numbers from the input array
+  array.map!{|number|number** 3}
+   
 end
 
 def first_cube_greater_than_500(array)
   # return the first number from the input array whose cube is greater than 500
+  array.find{|number|number ** 3 > 500}
 end
 
 def all_cubes_greater_than_500(array)
   # return all the numbers from the input array whose cube is greater than 500
+  array.map{|number|number ** 3 > 500}
+  #  binding.pry
+  #  us
 end
 
 def sum(array)
   # return the sum of all integers from the input array
+  array.reduce{|sum, num| sum += num}
 end
 
 def average_value(array)
